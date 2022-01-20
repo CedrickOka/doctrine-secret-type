f0690e9aceed454c95ae23863f2ebecd13d780d9
Cedrick Oka Baidai <cedric.baidai@veone.net>
2022-01-20 10:17:09 +0000

    Sanitize code

--- src/Types/SecretType.php
+++ src/Types/SecretType.php
@@ -1,50 +1,52 @@
 <?php
+
 namespace Oka\Doctrine\Types;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\TextType;
 
 /**
- * 
  * @author Cedrick Oka Baidai <okacedrick@gmail.com>
- * 
  */
 class SecretType extends TextType
 {
     use SecretTypeTrait;
-	
-	/**
-	 * {@inheritDoc}
-	 * @see \Doctrine\DBAL\Types\Type::convertToDatabaseValue()
-	 */
-	public function convertToDatabaseValue($value, AbstractPlatform $platform)
-	{
-		if (true === empty($value)) {
-			return null;
-		}
-		
-		return parent::convertToDatabaseValue($this->encrypt($value), $platform);
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 * @see \Doctrine\DBAL\Types\TextType::convertToPHPValue()
-	 */
-	public function convertToPHPValue($value, AbstractPlatform $platform)
-	{
-		if (true === empty($value)) {
-			return null;
-		}
-		
-		return $this->decrypt(parent::convertToPHPValue($value, $platform));
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 * @see \Doctrine\DBAL\Types\TextType::getName()
-	 */
-	public function getName()
-	{
-		return 'secret';
-	}
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see \Doctrine\DBAL\Types\Type::convertToDatabaseValue()
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (true === empty($value)) {
+            return null;
+        }
+
+        return parent::convertToDatabaseValue($this->encrypt($value), $platform);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see \Doctrine\DBAL\Types\TextType::convertToPHPValue()
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (true === empty($value)) {
+            return null;
+        }
+
+        return $this->decrypt(parent::convertToPHPValue($value, $platform));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see \Doctrine\DBAL\Types\TextType::getName()
+     */
+    public function getName()
+    {
+        return 'secret';
+    }
 }
--- src/Types/SecretTypeTrait.php
+++ src/Types/SecretTypeTrait.php
@@ -1,12 +1,11 @@
 <?php
+
 namespace Oka\Doctrine\Types;
 
 use Doctrine\DBAL\Types\ConversionException;
 
 /**
- * 
  * @author Cedrick Oka Baidai <okacedrick@gmail.com>
- * 
  */
 trait SecretTypeTrait
 {
@@ -14,81 +13,84 @@ trait SecretTypeTrait
      * @var string
      */
     private $privateKeyPath;
-    
+
     /**
      * @var string
      */
     private $publicKeyPath;
-    
+
     /**
      * @var string
      */
     private $passPhrase;
-	
-    public function setPrivateKeyPath(string $privateKeyPath) :self
+
+    public function setPrivateKeyPath(string $privateKeyPath): self
     {
         $this->privateKeyPath = $privateKeyPath;
+
         return $this;
     }
-    
-    public function setPublicKeyPath(string $publicKeyPath) :self
+
+    public function setPublicKeyPath(string $publicKeyPath): self
     {
         $this->publicKeyPath = $publicKeyPath;
+
         return $this;
     }
-    
-    public function setPassPhrase(string $passPhrase) :self
+
+    public function setPassPhrase(string $passPhrase): self
     {
         $this->passPhrase = $passPhrase;
+
         return $this;
     }
-    
-    protected function encrypt($data) :?string
+
+    protected function encrypt($data): ?string
     {
         $output = '';
         $encrypted = null;
         $maxlength = 256;
-        
+
         $fp = fopen($this->publicKeyPath, 'r');
         $publicKey = fread($fp, filesize($this->publicKeyPath));
         fclose($fp);
-        
+
         $key = openssl_get_publickey($publicKey);
-        
+
         while ($data) {
             if (false === openssl_public_encrypt(substr($data, 0, $maxlength), $encrypted, $key)) {
                 throw ConversionException::conversionFailedSerialization($data, $this->getName(), openssl_error_string());
             }
-            
+
             $output .= $encrypted;
             $data = substr($data, $maxlength);
         }
-        
+
         return base64_encode($output);
     }
-    
-    protected function decrypt($data) :?string
+
+    protected function decrypt($data): ?string
     {
         $output = '';
         $decrypted = null;
         $maxlength = 512;
-        
+
         $fp = fopen($this->privateKeyPath, 'r');
         $privateKey = fread($fp, filesize($this->privateKeyPath));
         fclose($fp);
-        
+
         $data = base64_decode($data);
         $key = openssl_get_privatekey($privateKey, $this->passPhrase);
-        
+
         while ($data) {
             if (false === openssl_private_decrypt(substr($data, 0, $maxlength), $decrypted, $key)) {
                 throw ConversionException::conversionFailedUnserialization($data, $this->getName(), openssl_error_string());
             }
-            
+
             $output .= $decrypted;
             $data = substr($data, $maxlength);
         }
-        
+
         return $output;
     }
 }
